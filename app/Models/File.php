fd0dc5ee532ab13e6696cbdefef8d0cd8c0c0c6e
Solano Jauregui <solanojauregui98@gmail.com>
2022-11-06 18:25:26 -0500

    Crea migracion y modelo File

---
+++ app/Models/File.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+
+class File extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'status',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('active', function (Builder $builder) {
+            $builder->where('status', 1);
+        });
+    }
+}
