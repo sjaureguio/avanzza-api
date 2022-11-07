9e59bf6e5eb967e3c62d8df74a7818fa107d38b0
Solano Jauregui <solanojauregui98@gmail.com>
2022-11-06 19:02:25 -0500

    Agrega seeder para la tabla users

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('123456'),
+        ]);
+
+        $user->createToken('access_token');
+    }
+}
