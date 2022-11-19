aeee5da1cae16b76309df0dbf0a777e1e46b26e6
Solano Jauregui <solanojauregui98@gmail.com>
2022-11-19 18:57:28 -0500

    Agrega metodos CRUD dentro  de FileController

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,21 +3,71 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\FileCollection;
+use App\Http\Requests\FileRequest;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Traits\FileStorage;
+use Illuminate\Support\Str;
 use App\Models\File;
 
 class FileController extends Controller
 {
+    use FileStorage;
+
     public function index(Request $request)
     {
-        $records = File::where('name', 'ILIKE', "%{$request->search}%")->latest();
+        return new FileCollection(File::latest()->paginate(20));
+    }
 
-        $data = new FileCollection($records->paginate(20));
+    public function store(FileRequest $request)
+    {
+        $files = $request->file('files');
+        
+        foreach ($files as $file) {
+            $fileName = Str::uuid()->toString() .'.'. $file->extension();
+
+            if ($this->uploadFile($file, $fileName, 'files')) {
+                File::create([
+                    'name' => $fileName,
+                ]);
+            }
+        }
 
         return response()->json([
             'success' => true,
             'message' => 'OK',
-            'data' => $data
-        ], 200);
+        ], 201);
+    }
+
+    public function getFile($id)
+    {
+        $file = File::findOrFail($id);
+
+        return $this->downloadStorage($file->name, 'files');
+    }
+
+    public function softDelete($id)
+    {
+        $file = File::findOrFail($id);
+        $file->update([
+            'status' => false,
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'OK',
+        ], 204);
+    }
+
+    public function destroy($id)
+    {
+        $file = File::findOrFail($id);
+
+        $this->deleteFile($file->name, 'files') && $file->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'OK',
+        ], 204);
     }
 }
