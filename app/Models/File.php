7853c1a578ad632290a596ba342cc92136117677
Solano Jauregui <solanojauregui98@gmail.com>
2022-11-19 19:01:41 -0500

    Agrega archivo LoginRequest para la validacion del lado del backend

--- app/Models/File.php
+++ app/Models/File.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class File extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     protected $fillable = [
         'name',
@@ -22,5 +23,10 @@ class File extends Model
         static::addGlobalScope('active', function (Builder $builder) {
             $builder->where('status', 1);
         });
+    }
+
+    public function getDownloadUrlAttribute()
+    {
+        return route('file.download', ['id' => $this->id]);
     }
 }
